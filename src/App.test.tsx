87ce27279bc70ceb36a052297b553ba72d473fd1
Charlie Dodds <charlesgdodds@hotmail.co.uk>
2022-02-12 14:47:22 +0000

    Update test and readme files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders header', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Spotify Recently Played/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('renders action buttons', () => {
+  render(<App />)
+  const actionButtons = screen.getAllByRole('button')
+  expect(actionButtons.length).toBe(3)
+})
